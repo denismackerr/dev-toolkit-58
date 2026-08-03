33cff921185f1b8fe0a4fc2f225f91a36a0ef561
denismackerr <denismackerr@inbox.lv>
2026-08-03 18:12:25 +0300

    update performance optimization for logger

---
+++ logger.py
@@ -0,0 +1,31 @@
+import logging
+
+class EnhancedLogger:
+    def __init__(self, name):
+        self.logger = logging.getLogger(name)
+        self.logger.setLevel(logging.DEBUG)
+        handler = logging.StreamHandler()
+        formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+        handler.setFormatter(formatter)
+        self.logger.addHandler(handler)
+
+    def debug(self, message):
+        self.logger.debug(message)
+
+    def info(self, message):
+        self.logger.info(message)
+
+    def warning(self, message):
+        self.logger.warning(message)
+
+    def error(self, message):
+        self.logger.error(message)
+
+    def critical(self, message):
+        self.logger.critical(message)
+
+# Example usage:
+if __name__ == '__main__':
+    log = EnhancedLogger('example_logger')
+    log.info('This is an info message')
+    log.error('This is an error message')
