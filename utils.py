a7f66a38a30fdabda78c6a0eb6dfb847d51479be
Denis Makerr <denismackerr@inbox.lv>
2026-08-12 10:15:43 +0300

    utility functions for crypto data handling

--- utils.py
+++ utils.py
@@ -1,24 +1,33 @@
 import json
+import requests
 
-def load_json(file_path):
-    with open(file_path, 'r') as file:
-        return json.load(file)
-
-
-def save_json(data, file_path):
-    with open(file_path, 'w') as file:
-        json.dump(data, file, indent=4)
+API_URL = 'https://api.coingecko.com/api/v3/'
 
 
-def merge_dicts(dict1, dict2):
-    result = dict1.copy()
-    result.update(dict2)
-    return result
+def fetch_crypto_data(crypto_id):
+    response = requests.get(f'{API_URL}coins/markets', params={'vs_currency': 'usd', 'ids': crypto_id})
+    if response.status_code == 200:
+        return json.loads(response.text)
+    return None
 
 
-def flatten_list(nested_list):
-    return [item for sublist in nested_list for item in sublist]
+def get_price(crypto_data):
+    if crypto_data and isinstance(crypto_data, list):
+        return crypto_data[0].get('current_price')
+    return None
 
 
-def filter_dict(input_dict, keys):
-    return {key: input_dict[key] for key in keys if key in input_dict}
+def convert_to_json(data):
+    try:
+        return json.dumps(data, indent=4)
+    except (TypeError, OverflowError):
+        return None
+
+
+def display_crypto_info(crypto_id):
+    data = fetch_crypto_data(crypto_id)
+    price = get_price(data)
+    if price:
+        print(f'The current price of {crypto_id} is ${price}.')
+    else:
+        print('Data not available.')
