51b0423b4060de27bb7192f23e6b34710089f68a
Denis Makerr <denismackerr@inbox.lv>
2026-08-12 10:14:25 +0300

    update logger setup with rotation

--- logger.py
+++ logger.py
@@ -1,41 +1,13 @@
 import logging
-import time
+from logging.handlers import RotatingFileHandler
 
-class Logger:
-    def __init__(self, name):
-        self.logger = logging.getLogger(name)
-        self.logger.setLevel(logging.INFO)
-        handler = logging.StreamHandler()
-        formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
-        handler.setFormatter(formatter)
-        self.logger.addHandler(handler)
+def setup_logger(log_file='app.log', max_bytes=10**6, backup_count=5):
+    logger = logging.getLogger(__name__)
+    logger.setLevel(logging.DEBUG)
+    handler = RotatingFileHandler(log_file, maxBytes=max_bytes, backupCount=backup_count)
+    formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+    handler.setFormatter(formatter)
+    logger.addHandler(handler)
+    return logger
 
-    def info(self, msg):
-        self.logger.info(msg)
-
-    def error(self, msg):
-        self.logger.error(msg)
-
-    def retry(func):
-        def wrapper(*args, **kwargs):
-            max_attempts = 3
-            for attempt in range(max_attempts):
-                try:
-                    return func(*args, **kwargs)
-                except Exception as e:
-                    if attempt < max_attempts - 1:
-                        time.sleep(2 ** attempt)
-                    else:
-                        raise e
-        return wrapper
-
-    @retry
-    def perform_network_operation(self):
-        # Simulated network operation
-        raise ConnectionError('Simulated network failure')
-
-logger = Logger(__name__)
-try:
-    logger.perform_network_operation()
-except Exception as ex:
-    logger.error(f'Operation failed: {ex}')
+logger = setup_logger()
