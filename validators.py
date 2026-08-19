437a5d5ab73b0a5b838a3d8d6cae707b3f143954
Denis Makerr <denismackerr@inbox.lv>
2026-08-19 10:14:53 +0300

    add validation methods

--- validators.py
+++ validators.py
@@ -1,28 +1,27 @@
 import re
 
-def is_valid_address(address: str) -> bool:
-    pattern = r'^0x[0-9a-fA-F]{40}$'
-    return bool(re.match(pattern, address))
+class Validator:
+    @staticmethod
+    def is_valid_address(address: str) -> bool:
+        pattern = r'^(0x)?[0-9a-fA-F]{40}$'
+        return bool(re.match(pattern, address))
 
+    @staticmethod
+    def is_valid_signature(signature: str) -> bool:
+        pattern = r'^[0-9a-fA-F]{130}$'
+        return bool(re.match(pattern, signature))
 
-def is_valid_amount(amount: float) -> bool:
-    return amount > 0
+    @staticmethod
+    def is_valid_amount(amount: float) -> bool:
+        return amount > 0
 
-
-def is_valid_transaction(data: dict) -> bool:
-    return all(key in data for key in ('from', 'to', 'amount')) and \
-           is_valid_address(data['from']) and \
-           is_valid_address(data['to']) and \
-           is_valid_amount(data['amount'])
-
-
-def validate_transactions(transactions: list) -> list:
-    return [tx for tx in transactions if is_valid_transaction(tx)]
-
-
-def extract_addresses(transactions: list) -> set:
-    addresses = set()
-    for tx in transactions:
-        addresses.add(tx['from'])
-        addresses.add(tx['to'])
-    return addresses
+    @staticmethod
+    def is_valid_transaction(tx: dict) -> bool:
+        return all(
+            [
+                Validator.is_valid_address(tx.get('from')),
+                Validator.is_valid_address(tx.get('to')),
+                Validator.is_valid_amount(tx.get('amount')),
+                Validator.is_valid_signature(tx.get('signature'))
+            ]
+        )
